d3db9bee2cd33500bdd91548234ba7f8a2923c02
SilverXuz <silver-xuz@mail.ru>
2023-08-06 20:55:18 +0300

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,94 @@
+"""
+Задание №5
+Создайте три (или более) отдельных классов животных.
+Например, рыбы, птицы и т.п.
+У каждого класса должны быть как общие свойства,
+например имя, так и специфичные для класса.
+Для каждого класса создайте метод, выводящий
+информацию специфичную для данного класса.
+"""
+
+"""
+Задание №6
+Доработайте задачу 5.
+Вынесите общие свойства и методы классов в класс
+Животное.
+Остальные классы наследуйте от него.
+Убедитесь, что в созданные ранее классы внесены правки.
+"""
+
+# Тема для меня пока сложная. Решение взял ваше, разбор посмотрел
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def get_info(self):
+        pass
+
+
+class Fish(Animal):
+    def __init__(self, name, depth):
+        super().__init__(name)
+        self.depth = depth
+
+    def get_info(self):
+        return f'Глубина обитания {self.name} = {self.depth} m'
+
+
+class Reptiles(Animal):
+    def __init__(self, name, length):
+        super().__init__(name)
+        self.length = length
+
+    def get_info(self):
+        return f'Длина тела {self.name} = {self.length} m'
+
+
+class Cat(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def get_info(self):
+        return f'Порода кошки {self.name} = {self.breed}'
+
+
+class Dog(Animal):
+    def __init__(self, name, breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def get_info(self):
+        return f'Порода собаки {self.name} = {self.breed}'
+
+
+class AnimalFactory:
+    def __init__(self):
+        self.animal_classes = {
+            'Fish': Fish,
+            'Reptiles': Reptiles,
+            'Cat': Cat,
+            'Dog': Dog
+        }
+
+    def create_animal(self, animal_type, *args):
+        if animal_type not in self.animal_classes:
+            raise ValueError("Invalid animal type")
+
+        animal_class = self.animal_classes[animal_type]
+        return animal_class(*args)
+
+
+if __name__ == "__main__":
+    factory = AnimalFactory()
+
+    fish = factory.create_animal('Fish', 'Goldfish', 0.3)
+    reptile = factory.create_animal('Reptiles', 'Snake', 1.5)
+    cat = factory.create_animal('Cat', 'Fluffy', 'Persian')
+    dog = factory.create_animal('Dog', 'Buddy', 'Labrador')
+
+    print(fish.get_info())
+    print(reptile.get_info())
+    print(cat.get_info())
+    print(dog.get_info())
